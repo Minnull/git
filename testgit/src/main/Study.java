554f752e4d7ca03ddf2294b480f778b9e2ebee62
mainnull <652518954@qq.com>
2019-02-06 23:50:59 +0800

    提交了第二次

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -6,5 +6,6 @@ public class Study {
         System.out.println("这是第一个git文件");
         System.out.println("第二行");
         System.out.printf("merge");
+        System.out.printf("这个是第二次");
     }
 }
