74e2b567a0027901ce0c9cfd3cc70020337b332d
mainnull <652518954@qq.com>
2019-02-06 23:51:28 +0800

    rebase

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -5,5 +5,6 @@ public class Study {
     public static void main(String[] args) {
         System.out.println("这是第一个git文件");
         System.out.println("第二行");
+        System.out.printf("rebase");
     }
 }
