4c705a2ff10506ecac46c5487a42065e110f56a5
mainnull <652518954@qq.com>
2019-02-07 00:33:21 +0800

    Revert "Merge branch 'dev-merge'"
    This reverts commit 678d3d305a2ae828f320d5fa00e4c165ca71b1c2, reversing
    changes made to 74e2b567a0027901ce0c9cfd3cc70020337b332d.
    
    # Conflicts:
    #	testgit/src/main/Study.java

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -6,10 +6,6 @@ public class Study {
         System.out.println("这是第一个git文件");
         System.out.println("第二行");
 
-
         System.out.printf("rebase");
-
-        System.out.printf("merge");
-        System.out.printf("这个是第二次");
     }
 }
