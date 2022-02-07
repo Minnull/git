30b8b42141d7fd6112e01e6fe9349a6ce1791c27
maweijie <652518954@qq.com>
2022-02-08 01:22:02 +0800

    add 22

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -8,5 +8,7 @@ public class Study {
         System.out.printf("rebase");
         System.out.printf("rebase");
         System.out.printf("rebase");
+        System.out.printf("rebase");
+
     }
 }
