954506532a89aeffdf6e46558a3916d67a7931e9
maweijie <652518954@qq.com>
2022-02-08 02:34:07 +0800

    add new 33

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -6,7 +6,7 @@ public class Study {
         System.out.println("这是第一个git文件");
         System.out.println("第二行");
         System.out.printf("6666");
-        System.out.printf("rebase");
+        System.out.printf("8888");
         System.out.printf("rebase");
         System.out.printf("rebase");
         System.out.printf("rebase");
