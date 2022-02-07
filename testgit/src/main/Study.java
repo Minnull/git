ed151ec18cfc9f0f2b0db29cdab9939e41997ccb
maweijie <652518954@qq.com>
2022-02-08 02:14:05 +0800

    add new 20

--- testgit/src/main/Study.java
+++ testgit/src/main/Study.java
@@ -5,7 +5,7 @@ public class Study {
     public static void main(String[] args) {
         System.out.println("这是第一个git文件");
         System.out.println("第二行");
-        System.out.printf("2");
+        System.out.printf("20");
         System.out.printf("4");
         System.out.printf("6");
         System.out.printf("8");
